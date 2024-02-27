90b49a4dbaf3c1c454af8c2148c395146e3de474
Giovana Nogueira <ongiovana2016@gmail.com>
2024-02-27 14:39:15 -0300

    feat: timer started

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { React } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Timer from "./Timer";
 
 function App() {
   return (
     <div>  
       <Header />
+      <Timer />
       <Footer />
     </div>
   );
---
+++ src/components/Timer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Timer() {
+  return (
+    <div className="timer-div">
+
+    <div className="time-div">
+      <h1>00:00</h1>
+    </div>
+
+    </div>
+  );
+}
+
+export default Timer;
